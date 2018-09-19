7a197a911b1362545c884e71733483404720ab5c
RyanJF <ryanjfreeman77@gmail.com>
2018-09-18 23:17:01 -0500

    trying out grid

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 var API_KEY = "QQx3yAB40lUloE1r1Oa2xgCTYCbXpOwb";
 
-var topics = ["movies", "music", "dnd", "vinyl records", "dogs", "discgolf", "javascript", "books", "kcchiefs", "sportingKC"];
+var topics = ["movies", "music", "dnd", "vinyl records", "dogs", "no", "cats", "books", "kc chiefs", "yes"];
 var buttonWrapperDiv = $("#button-wrapper");
-
+var gifWrapperMain = $("#wrapper");
 
 
 $(document).ready(function () {
     var height = $(document).height();
-    $("#body-wrapper").height(height + "px");
+    // $("#body-wrapper").height(height + "px");
     makeButton();
 
     $("#search-btn").on("click", function (event) {
@@ -18,24 +18,44 @@ $(document).ready(function () {
         makeButton();
     });
 
-    $(document).on("click", ".search-btn", runAPI)
+    $(document).on("click", ".search-btn", function(){
+        gifWrapperMain.empty();
+        var dataTerm = $(this).data("term");
+        console.log($(this).data("term"));
+        runAPI(dataTerm, 10);
+    });
 
 
 });
 
-function runAPI() {
-    var search = $(this).data("term");
-    var limit = 10;
+function runAPI(q, l) {
+    var search = q;
+    var limit = l;
     var queryURL = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=${limit}`
-
     $.ajax({
         url: queryURL,
         method: "GET",
-    }).then(function (response) {
-        console.log(response);
+    }).then(function (res) {
+        console.log(queryURL);
+        console.log(res);
+        var gifWrapperDiv = $("<div>");
+        for (var i = 0; i < l; i++) {
+            console.log(res.data[i]);
+            console.log(res.data[i].images.fixed_width_still.url);
+            var tempGifDiv = $("<div>");
+            var img = $("<img>");
+            img.attr("src", res.data[i].images.fixed_width_still.url).attr("id", "gif-img-" + i); 
+            tempGifDiv.append(img);
+            gifWrapperDiv.append(tempGifDiv);
+        }
+        gifWrapperMain.append(gifWrapperDiv);
     });
 }
 
+
+// function makeGifDiv(gifUrl) {
+//     var gifWrapperDiv = $("<div>");
+// }
 
 
 
